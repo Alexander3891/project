73667409b287cae74b5e2d38cef85f5a4bdf7949
Alex <sanichen350@gmail.com>
2021-04-02 11:29:46 +0300

    my lesson number 12

--- script3.js
+++ script3.js
@@ -1,34 +1,21 @@
 "use strict";
 
-// let name1 = "Ivan";
+let numberOfFilms = prompt("Сколько фильмов вы уже посмтрели?");
 
-// console.log(`Привет, ${name1}`);
+const personaMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+}
 
-// console.log(4 + +"5");
-
-
-let incr = 10,
-    decr = 10;
-// инкримент
-// incr++; 
-// декримент
-// decr--; 
-// console.log(++incr); // не изменится
-// console.log(--decr); 
-// console.log(incr++); // не изменится
-// console.log(decr--);
-// console.log(5 % 2);
-// console.log(2 * 4 == "8");
-// const isChecked = true,
-//       isClose = false; 
-
-// console.log(isChecked && !isClose);
-// console.log(4 + 2 * 2 == !"8");  //false
-// console.log(2 + 2 * 2 != 8);  //true
-console.log("2 + 2 * 2" !=  '6');  //false 
-// console.log(2 + 2 * 2 !== '6');  //true
-
-
-
-
+let a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?"),
+    d = prompt("На сколько оцените его?");
+    personaMovieDB.movies[a] = b;
+    personaMovieDB.movies[c] = d;
+console.log(personaMovieDB);
+console.log(personaMovieDB.movies);
 
