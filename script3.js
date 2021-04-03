64448680b73ea99eb6173bda2879887fcda8af9e
Alex <sanichen350@gmail.com>
2021-04-03 15:58:47 +0300

    lesson number 13

--- script3.js
+++ script3.js
@@ -1,21 +1,77 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмтрели?");
+// let numberOfFilms = prompt("Сколько фильмов вы уже посмтрели?");
 
-const personaMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
-}
+// const personaMovieDB = {
+//     count : numberOfFilms,
+//     movies : {},
+//     actors : {},
+//     genres : [],
+//     privat : false
+// }
 
-let a = prompt("Один из последних просмотренных фильмов?"),
-    b = prompt("На сколько оцените его?"),
-    c = prompt("Один из последних просмотренных фильмов?"),
-    d = prompt("На сколько оцените его?");
-    personaMovieDB.movies[a] = b;
-    personaMovieDB.movies[c] = d;
+// let a = prompt("Один из последних просмотренных фильмов?"),
+//     b = prompt("На сколько оцените его?"),
+//     c = prompt("Один из последних просмотренных фильмов?"),
+//     d = prompt("На сколько оцените его?");
+//     personaMovieDB.movies[a] = b;
+//     personaMovieDB.movies[c] = d;
 // console.log(personaMovieDB);
 // console.log(personaMovieDB.movies);
+
+// if (1) {
+//     console.log('true');
+// }
+// else {
+//     console.log('false');
+// }
+
+// const num = 50  ;
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num >100) {
+//     console.log('Very big');
+// } else  {
+//     console.log('Good');
+// }
+
+// (num === 50) ? console.log('Good') : console.log("Error");
+  
+// let number = 500;
+// switch (number) {
+//     case 49: 
+//         console.log('Error');
+//         break;
+//     case 50:
+//         console.log('Good');
+//         break;
+//     case 55:
+//         console.log('Very big number');
+//         break;
+//     case 40:
+//         console.log('Very little number');
+//         break;
+//     default:
+//         console.log('It isnt right number');
+//         break;
+// }
+
+let num = 100;
+
+switch (num) {
+    case 50:
+        console.log('Good');
+        break;
+    case 55:
+        console.log('Very big number');
+        break;
+    case 45:
+        console.log('Very little number');
+        break;
+    default:
+        console.log('It isnt right number');
+        break;
+}
+
+
 
