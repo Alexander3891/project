56500e06a590ebb590411313c0d6409e49e43de0
Alex <sanichen350@gmail.com>
2021-04-19 18:08:33 +0300

    taby continue

--- taby/js/script.js
+++ taby/js/script.js
@@ -9,18 +9,36 @@ window.addEventListener('DOMContentLoaded', () => {
 // скрываем все не нужные табы
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+
         });
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     }
  //  показывает нужный таб
-    function showTabContent(i) {
-        tabsContent[i].style.display = 'block';
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
         tabs[i].classList.add('tabheader__item_active');
     }
 
     hideTabContent();
-    showTabContent(0);
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                     hideTabContent();
+                    showTabContent(i);
+                
+                }
+            });
+        }
+    });
 });
