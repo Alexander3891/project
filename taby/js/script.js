da18773bb00fb68b88d2b8fe40e342def9ecaf68
Alex <sanichen350@gmail.com>
2021-04-23 16:12:28 +0300

    taby

--- taby/js/script.js
+++ taby/js/script.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
