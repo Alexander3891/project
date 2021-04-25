f58cd428ab8577fc75d8674f3f3226418f9813f9
Alex <sanichen350@gmail.com>
2021-04-25 14:39:19 +0300

    data

---
+++ data/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const now = new Date();
+// console.log(now);
+
+console.log(now.getFullYear());// год
+console.log(now.getMonth());// месяц
+console.log(now.getDate());// дата
+console.log(now.getHours());// часы
+console.log(now.getMinutes());//минуты
+console.log(now.getSeconds());//секунды
+console.log(now.getMilliseconds());//милисекунды
+
+// const noow = new Date(2020, 5 ,1 ,20);
+// console.log(noow);
+
+// const nnow = new Date(0); // в милисикундах
+// console.log(nnow);
+
