1a37ee2148e235c4ca6c2b29e29b355c47722636
Alex <sanichen350@gmail.com>
2021-05-02 16:38:16 +0300

    ajax

--- JSON/script.js
+++ JSON/script.js
@@ -2,10 +2,17 @@
 
 const persne = {
     name: 'Alex',
-    tel: '+7444444'
+    tel: '+7444444',
+    parents: {
+    mom: 'Olga',
+    dad: 'Mike'
+}
 };
 // получаем JSON файл
 console.log(JSON.stringify(persne));
 
 // получаем обьект с JSON файла
 console.log(JSON.parse(JSON.stringify(persne)));
+
+// копия обьекта с помощью JSON
+const clone = JSON.parse(JSON.stringify(persne));
