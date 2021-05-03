d67cd2ce309775ccc195230691d02904083cd2e5
Alex <sanichen350@gmail.com>
2021-05-03 16:25:19 +0300

    ajax continue part two

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -24,8 +24,8 @@ inputRub.addEventListener('input', () => {
                 
     // });
 //  ===================== ИЛИ =====================
-request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
+request.addEventListener('load', () => {
+        if (request.status === 200) {
             console.log(request.response);
             const data = JSON.parse(request.response);
             inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
