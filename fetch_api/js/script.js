db00331abbce8b2de6ce5e36608211f82ed67d39
Alex <sanichen350@gmail.com>
2021-05-07 12:08:18 +0300

    fetch

--- fetch_api/js/script.js
+++ fetch_api/js/script.js
@@ -1,58 +1,23 @@
 'use strict';
 
-console.log('Запрос данных...');
+// запускать на сервере 
 
-const req = new Promise(function(resolve, reject){
-    setTimeout(()=> {
-        console.log('Подготовка данных...');
-    
-        const product = {
-            name: 'TV',
-            price: 2000    
-        };
-        //для ошибок
-        // reject();
-    
-        resolve(product);
-    },2000);  
-});
+//GET получае данные
+ fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json()) // (возвращается промис)получаем ответ в формате json и преобразуем его в обьект
+    .then(json => console.log(json));
+//POST добавляем данные
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({ name: 'Alex' }),
+    headers: {
+        'Content-type': 'application/json'
+}
+})
+    .then(response => response.json()) // (возвращается промис)получаем ответ в формате json и преобразуем его в обьект
+    .then(json => console.log(json));
 
-req.then((product)=> {
-    return  new Promise((resolve,reject)=> {
-        setTimeout(()=> {
-            product.status = 'order';
-            resolve(product);
-        },2000); 
-    });
-}).then(product => {  
-      product.modify = true;
-      return product;
-}).then(product => {
-    console.log(product);
-}).catch(()=>{  // если что-то пошло не так
-console.error ('Произошла ошибка');
-}).finally(()=>{   //  можно очистить форму после отправки данных
-   console.log('Finally'); 
-});
 
-// одновременное выполнение двух промисов
-const test = time => {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(),time)      
-    });
-};
-
-// test(1000).then(() => console.log('1000 ms'));
-// test(2000).then(() => console.log('2000 ms'));
- 
-// Promise.all([test(1000), test(2000)]).then(() => {
-    // console.log('All');
-// });
-
-// промис выполнится сразукогда выполнится первое действие
-Promise.race([test(1000), test(2000)]).then(() => {
-    console.log('All');
-});
 
 
 
