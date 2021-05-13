88bb1d951fcd53688c387fd01d4cceaf2a4b53c5
Alex <sanichen350@gmail.com>
2021-05-13 12:02:13 +0300

    localStorage

---
+++ localStorage/script.js
@@ -0,0 +1,54 @@
+'use strict';
+// // записать данные в localStorage
+// localStorage.setItem('number', '5');
+
+// // получить элемент из localStorage
+// console.log(localStorage.getItem('number'));
+
+// // удалить элемент из localStorage
+// localStorage.removeItem('number');
+
+// // очистить полностью localStorage
+// localStorage.clear();
+
+//=========================================================
+
+const checkbox = document.querySelector('#checkbox'),
+    form = document.querySelector('form'),
+    change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+ if (localStorage.getItem('bg') === 'changed') {
+        form.style.backgroundColor = 'red';
+    }
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+
+const persone = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone); 
+localStorage.setItem('alex', 'serializedPersone');
+
+console.log(JSON.parse(localStorage.getItem('alex')));
+
+
+
+
