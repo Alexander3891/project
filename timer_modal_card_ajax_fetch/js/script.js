19fba2bb18f5aac640941b52ae2706b65884bd24
Alex <sanichen350@gmail.com>
2021-05-13 09:21:13 +0300

    navigation for slide

--- timer_modal_card_ajax_fetch/js/script.js
+++ timer_modal_card_ajax_fetch/js/script.js
@@ -508,6 +508,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ======================  Slide-2 ===================
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),// для точек
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -535,7 +536,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
     slides.forEach(slide => {
         slide.style.width = width;
-    });  
+    });
+    
+    // ============ точки =================
+    slider.style.position = 'relative';
+    // элемет для точек
+    const indicators = document.createElement('ol'),
+        //для активности точек
+        dots = [];
+    // стили для точек  
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `;
+// помещаем обёртку на страницу
+    slider.append(indicators);
+
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+        box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 40px;
+    height: 4px;
+    margin-right: 10px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+        `;
+// ========= активность точек ===============
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+    
+    
+
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
@@ -555,7 +610,11 @@ if (slideIndex == slides.length) {
         } else {
             current.textContent = slideIndex;
             
-        }    
+        }
+        // для активности точек
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = '1';  
+
     });
 
     prev.addEventListener('click', () => {
@@ -579,20 +638,11 @@ if (slideIndex == slides.length) {
             current.textContent = slideIndex;
             
         }
+        // для активности точек
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = '1';  
+
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
