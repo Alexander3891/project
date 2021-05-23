0c816ed9bfafd60b81908872b10d96247332ee7b
Alex <sanichen350@gmail.com>
2021-05-23 18:32:54 +0300

    webpack_continue

---
+++ webpack/src/js/script.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+//===============  webpack ================
+const myModule = require('./main');
+const myModuleInstanse = new myModule();
+
+myModuleInstanse.hello();
+myModuleInstanse.goodbye();
+
