eb9ef77cebb826b5944cbe7ee14aaf4ab5cfeda3
Alex <sanichen350@gmail.com>
2021-05-31 16:15:12 +0300

    error finish

--- errors/script.js
+++ errors/script.js
@@ -2,8 +2,17 @@
 
 try {
     console.lo('Normal');
-} catch (e) {
-    console.log('Error');
+} catch (error) {
+    console.log(error);
+    console.log(error.message);
+    console.log(error.stack);
+
+} finally {
+    
+    // always work in finish
 }
-    console.log('Continue');
+
+
+// if error code continue to work!!!
+console.log('Continue');
  
