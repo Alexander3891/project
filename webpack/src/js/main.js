f8bee80c8a4222a6a14f5165952b8c23d7bd2a41
Alex <sanichen350@gmail.com>
2021-06-01 09:08:26 +0300

    jquey start

--- webpack/src/js/main.js
+++ webpack/src/js/main.js
@@ -1,13 +1,26 @@
-// 'use strict';
+'use strict';
 
 
-function myModule(){
-    this.hello = function() {
-        console.log('hello');
-    };
+// function myModule(){
+//     this.hello = function() {
+//         console.log('hello');
+//     };
 
-    this.goodbye = function () {
-        console.log('bye!');
-    };
+//     this.goodbye = function () {
+//         console.log('bye!');
+//     };
+// }
+// module.exports = myModule;
+
+export let one =1;
+
+let two = 2;
+export {two};
+
+// export function sayHi() {
+//     console.log('Hello');
+// }
+// ЭКСПОРТ ПО УМОЛЧАНИЮ
+export default function sayHi() {
+    console.log('Hello');
 }
-module.exports = myModule;
--- webpack/src/js/script.js
+++ webpack/src/js/script.js
@@ -1,9 +1,25 @@
 // 'use strict';
 
 //===============  webpack ================
-const myModule = require('./main');
-const myModuleInstanse = new myModule();
+// const myModule = require('./main');
+// const myModuleInstanse = new myModule();
 
-myModuleInstanse.hello();
-myModuleInstanse.goodbye();
+// myModuleInstanse.hello();
+// myModuleInstanse.goodbye();
+
+import {one,two} from './main.js';
+console.log(`${one} and ${two}`);
+
+
+import {one as first} from './main.js';// переименовываем
+console.log(first);
+
+
+import * as data from './main.js'; // єкспортировать всё
+console.log(`${data.one} and  ${data.two}`);
+//  data.sayHi();
+
+// ЭКСПОРТ ПО УМОЛЧАНИЮ
+import sayHi from './main.js';
+ sayHi();
 
